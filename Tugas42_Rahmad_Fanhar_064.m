bfb9690de246d72b6e84e26ad6f6bc8bb5ae0dd7
Rahmad Fanhar <rahmadfanhar876@gmail.com>
2023-10-28 01:44:56 +0700

    selesai menjawab soal 4.2 bagian a,b, dan c

--- Tugas42_Rahmad_Fanhar_064.m
+++ Tugas42_Rahmad_Fanhar_064.m
@@ -12,4 +12,36 @@ disp('Jawab Soal 4.2')
 disp('===================================================================')
 disp('Matriks identitas dengan dimensi 5 x 5')
 
+% (a.) Matriks Identitas dengan Dimensi 5 x 5
+% A = eye(n)  dimana n adalah nilai dimensi
+
+A = eye(5,5)
+
+disp('===================================================================')
+disp('Matriks Dimensi 3 x 3 dengan Elemen-elemen Semuanya Bernilai 0(nol)')
+
+% (b.) Matriks Dimensi 3 x 3 dengan Elemen Bernilai 0(nol)
+% B = zeros(n)   dimana n adalah nilai dimensi
+
+B = zeros(3,3)
+
+disp('===================================================================')
+disp('Matriks Dimensi 4 x 4 dengan Elemen-elemen Semuanya Bernilai 0(nol)')
+
+% (c.) Matriks Dimensi 3 x 3 dengan Elemen Bernilai 1(Satu)
+% C = ones(n)   dimana n adalah nilai dimensi
+
+C = ones(4,4)
+
+disp('===================================================================')
+disp('Matriks Dimensi 3 x 4 Dimana Elemen-elemen Terdistribusi')
+disp('Antara 0(nol) dan 1(satu) (default n = m)')
+
+% (d.) Matriks Dimensi 3 x 4 dengan Elemen Terdistribusi Antara 0(nol)
+% dan 1(satu) (default n = m)
+% D = zeros(n)   dimana n adalah nilai dimensi
+
+
+
+
 
