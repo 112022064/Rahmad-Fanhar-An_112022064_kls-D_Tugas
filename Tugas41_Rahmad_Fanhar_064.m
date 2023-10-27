6ba3f5ea09dc7d0d27555f1dc1c488a02ba7682a
Rahmad Fanhar <rahmadfanhar876@gmail.com>
2023-10-28 00:12:40 +0700

    selesai sampai soal d

--- Tugas41_Rahmad_Fanhar_064.m
+++ Tugas41_Rahmad_Fanhar_064.m
@@ -7,3 +7,61 @@ disp('Kelas     : D')
 disp('===================================================================')
 
 disp('Jawab Soal 4.1')
+
+%4.1
+
+%Diketahui Matriks A
+
+A = [ 1 2 3; 2 1 1; 3 2 1]
+
+%Diketahui Matriks B
+
+B = [ 4 4 5; 6 1 2; 3 5 5]
+
+
+%penyelesaian dari soal A sampai Z
+disp('===================================================================')
+disp('determinan Matriks')
+
+%soal (a.) Determinan Matriks A dan Matriks B
+% () = det(_)
+
+a_A = det(A)
+a_B = det(B)
+
+disp('===================================================================')
+disp('Ukuran Matriks')
+
+%soal (b.) Ukuran dari Matriks A dan Matriks B
+% () = size(_)
+
+b_A = size(A)
+b_B = size(B)
+
+disp('===================================================================')
+disp('Trace Matriks')
+
+%soal (c.) Trace dari Matriks A dan Matriks B
+% () = trace(_)
+
+c_A = trace(A)
+c_B = trace(B)
+
+disp('===================================================================')
+disp('Norm Matriks')
+
+%Norm Matriks A dan Matriks B
+% () = norm(_)
+
+d_A = norm(A)
+d_B = norm(B)
+
+
+
+
+
+
+
+
+
+ 
