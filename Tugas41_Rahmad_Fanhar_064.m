25dfbfb913cf98056cd6cbf26931e6a3550efe2f
Rahmad Fanhar <rahmadfanhar876@gmail.com>
2023-10-28 01:16:52 +0700

    Selesai Menjawab Soal 4.1

--- Tugas41_Rahmad_Fanhar_064.m
+++ Tugas41_Rahmad_Fanhar_064.m
@@ -50,11 +50,195 @@ c_B = trace(B)
 disp('===================================================================')
 disp('Norm Matriks')
 
-%Norm Matriks A dan Matriks B
+%soal (d.) Norm Matriks A dan Matriks B
 % () = norm(_)
 
 d_A = norm(A)
 d_B = norm(B)
+
+disp('===================================================================')
+
+%(e.)
+
+e_C = A+B
+
+disp('===================================================================')
+
+%(f.)
+
+f_C = A-B
+
+disp('===================================================================')
+
+%(g.)
+
+g_C = A*B
+
+disp('===================================================================')
+
+%(h.)
+
+h_C = A.*B
+
+disp('===================================================================')
+
+%(i.)
+
+i_C = A^2
+
+disp('===================================================================')
+
+%(j.)
+
+j_C = A.^2
+
+disp('===================================================================')
+disp('Transpose Matriks')
+
+%soal (k.) Transpose dari Matriks A dan Matriks B
+% () = (_)'
+
+k_A = A'
+k_B = B'
+
+disp('===================================================================')
+
+%(L.)
+
+l_C = A./B
+
+disp('===================================================================')
+
+%s(m.)
+
+m_C = A.\B
+
+disp('===================================================================')
+
+%(n.)
+
+n_C = A/B
+
+disp('===================================================================')
+disp('Invers Matriks')
+
+%soal (o.) Invers dari Matriks A dan Matriks B
+% () = inv(_)
+
+o_A = inv(A)
+o_B = inv(B)
+
+disp('===================================================================')
+
+%soal (p.) Null Matriks A
+
+p_C = null(A)
+
+disp('===================================================================')
+
+%soal (q.) Orthonormal Matriks A
+
+q_C = orth(A)
+
+disp('===================================================================')
+
+%soal (r.) Reduced Row Echelon Form Matriks A
+
+r_C = rref(A)
+
+disp('===================================================================')
+disp('Nilai Eigen Matriks')
+
+%soal (s.) Nilai Eigen dari Matriks A dan Matriks B
+% () = eig(_)
+
+s_A = eig(A)
+s_B = eig(B)
+
+disp('===================================================================')
+disp('Singular Value Decomposition Matriks')
+
+%soal (t.) Singular dari Matriks A dan Matriks B
+% () = svd(_)
+
+t_A = svd(A)
+t_B = svd(B)
+
+disp('===================================================================')
+disp('Upper Triangular Part of Matrix')
+
+%soal (u.) Matriks Segitiga Atas dari Matriks A dan Matriks B
+% () = triu(_)
+
+u_A = triu(A)
+u_B = triu(B)
+
+disp('===================================================================')
+disp('Lower Triangular Part of Matrix')
+
+%soal (v.) Matriks Segitiga Bawah dari Matriks A dan Matriks B
+% () = tril(_)
+
+v_A = tril(A)
+v_B = tril(B)
+
+disp('===================================================================')
+disp('Nilai Maximum dari Elemen-elemen Matriks')
+
+%soal (w.) Nilai Maximum dari Elemen-elemen Matriks A dan Matriks B
+% () = max(max(_))
+
+w_A = max(max(A))
+w_B = max(max(B))
+
+disp('===================================================================')
+disp('Nilai MInimum dari Elemen-elemen Matriks')
+
+%soal (x.) Nilai Minimum dari Elemen-elemen Matriks A dan Matriks B
+% () = min(min(_))
+
+x_A = min(min(A))
+x_B = min(min(B))
+
+disp('===================================================================')
+disp('Jumlah Kolom dari Elemen-elemen Matriks')
+
+%soal (y.) Jumlah Kolom dari Elemen-elemen Matriks A dan Matriks B
+% () = length(_)
+
+y_A = length(A)
+y_B = length(B)
+
+disp('===================================================================')
+disp('Diagonal dari Elemen-elemen Matriks')
+
+%soal (z.) Diagonal dari Elemen-elemen Matriks A dan Matriks B
+% () = diag(_)
+
+z_A = diag(A)
+z_B = diag(B)
+
+disp('===================================================================')
+disp('===================================================================')
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
