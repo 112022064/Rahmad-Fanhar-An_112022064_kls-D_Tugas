7631ebeb871c38abfd793f74bb60462ab36be840
Rahmad Fanhar <rahmadfanhar876@gmail.com>
2023-10-28 01:26:47 +0700

    Create Tugas42_Rahmad_Fanhar_064.m
    menambahkan jawaban untuk soal 4.2

---
+++ Tugas42_Rahmad_Fanhar_064.m
@@ -0,0 +1,15 @@
+clear all
+clc
+
+disp('Nama      : Rahmad Fanhar An')
+disp('Nim       : 112022064')
+disp('Kelas     : D')
+disp('===================================================================')
+
+disp('Jawab Soal 4.2')
+
+%penyelesaian dari soal 4.2
+disp('===================================================================')
+disp('Matriks identitas dengan dimensi 5 x 5')
+
+
