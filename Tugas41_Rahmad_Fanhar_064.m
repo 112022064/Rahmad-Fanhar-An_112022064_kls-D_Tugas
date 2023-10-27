ba106be8bdd9912f48ea282d2a2fb035c4e5fccc
Rahmad Fanhar <rahmadfanhar876@gmail.com>
2023-10-27 23:39:56 +0700

    Create Tugas41_Rahmad_Fanhar_064.m
    buat ulang karena file kemarin hilang gara gara ganti nama file

---
+++ Tugas41_Rahmad_Fanhar_064.m
@@ -0,0 +1,8 @@
+clear all
+clc
+
+disp('Nama      : Rahmad Fanhar An')
+disp('Nim       : 112022064')
+disp('Kelas     : D')
+disp('===================================================================')
+
